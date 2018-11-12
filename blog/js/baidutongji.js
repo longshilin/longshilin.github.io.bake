b741bfec49aa6f3a9121f2428e81fd664fd52161
longshilin <583297550@qq.com>
2018-11-12 12:58:25 +0000

    Site updated：Mon Nov 12 12:58:25 UTC 2018

---
+++ blog/js/baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a41cf071b9e4c8541873368bedab6e0e";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+</script>
