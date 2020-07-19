b667d270b97a7e4e21ec5e6419fa6cadb156c32e
longshilin <583297550@qq.com>
2020-07-19 16:46:09 +0000

    Site updated：Sun Jul 19 16:46:09 UTC 2020

--- blog/js/baidutongji.js
+++
@@ -1,9 +0,0 @@
-<script>
-  var _hmt = _hmt || [];
-  (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?a41cf071b9e4c8541873368bedab6e0e";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-  })();
-</script>
